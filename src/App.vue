062da2f3db5ffb08d40460c5b2e35153ee51150c
OliveLeb <lebel.olivier@wanadoo.fr>
2021-06-16 11:49:52 +0200

    profilCard component with dynamic props

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <h1>Hello World !</h1>
-  <Avatar name='CESAR' image='https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHwyfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60' />
+  <Avatar name='CESAR' badgeColor='#5ad539' image='https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHwyfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60' />
+  <ProfilCard :status='true' name='Ricky Park' 
+  img='https://randomuser.me/api/portraits/women/79.jpg' 
+  city='New York' job='Front end developper'
+  :skills="['UI / UX','HTML','CSS','JavaScript','React','Node']"
+  />
 </template>
 
 <script>
 import Avatar from './components/Avatar.vue';
+import ProfilCard from './components/ProfilCard.vue';
 
 export default {
     name: 'App',
     components: {
       Avatar,
+      ProfilCard,
     }
 }
 </script>
 
 <style>
-    h1 {
+    body{
       font-family: Arial, sans-serif;
+    }
+    h1 {
       text-align: center;
       color: #2c3e50;
       margin-top: 60px;
